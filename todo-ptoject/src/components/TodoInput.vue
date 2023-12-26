e633c20da2e5b5d58c24e86c34e71e80a9072d84
ygvbhy <ygvbhy@naver.com>
2023-12-26 23:04:23 +0900

    input component 디자인 및 기본 작성

--- todo-ptoject/src/components/TodoInput.vue
+++ todo-ptoject/src/components/TodoInput.vue
@@ -1,11 +1,57 @@
 <template>
-  <div>Input</div>
+  <div class="inputBox shadow">
+    <input type="text" v-model="newTodoItem" @keyup.enter="addTodo()" />
+    <span class="addContainer">
+      <i class="fas fa-plus addBtn" @click="addTodo()"></i>
+    </span>
+  </div>
 </template>
 
 <script>
 export default {
   name: "TodoInput",
+  data() {
+    return {
+      newTodoItem: "",
+    };
+  },
+  methods: {
+    addTodo() {
+      localStorage.setItem(this.newTodoItem, this.newTodoItem);
+      this.clearInput();
+    },
+    clearInput() {
+      this.newTodoItem = "";
+    },
+  },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+input:focus {
+  outline: none;
+}
+.inputBox {
+  background: white;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 5px;
+}
+.inputBox input {
+  width: 80%;
+  border-style: none;
+  font-size: 0.9rem;
+}
+.addContainer {
+  float: right;
+  background: linear-gradient(to right, #6478fb, #8763fb);
+  display: block;
+  width: 3rem;
+  border-radius: 0 5px 5px 0;
+  cursor: pointer;
+}
+.addBtn {
+  color: white;
+  vertical-align: middle;
+}
+</style>
--- todo-ptoject/src/components/TodoList.vue
+++ todo-ptoject/src/components/TodoList.vue
@@ -1,10 +1,28 @@
 <template>
-  <div>Todo List</div>
+  <div>
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
   name: "TodoList",
+  data() {
+    return {
+      todoItems: {},
+    };
+  },
+  created() {
+    if (localStorage.length > 0) {
+      for (let i = 0; i < localStorage.length; i++) {
+        console.log("test");
+      }
+    }
+  },
 };
 </script>
 
